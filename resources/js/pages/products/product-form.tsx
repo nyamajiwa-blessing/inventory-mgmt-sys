eb2178830aaa039cb5c53f934b9114ba480fbc5e
goodGuyFrost <128358852+nyamajiwa-blessing@users.noreply.github.com>
2025-08-24 14:52:41 +0200

    Add CustomTextarea component and update ProductForm with new UI elements

--- resources/js/pages/products/product-form.tsx
+++ resources/js/pages/products/product-form.tsx
@@ -1,3 +1,6 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
@@ -15,7 +18,27 @@ export default function ProductForm() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Manage Products" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Add New Product</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <form className="flex flex-col gap-4" autoComplete='off'>
+                            <div className="grid gap-6">
+                                {/* Product Name */}
+                                <div className="grid gap-2">
+                                    <Label htmlFor="name">Product Name</Label>
+                                    <Input id='name' name='name' type='text' placeholder='Enter product name' autoFocus tabIndex={1} />
+                                </div>
+                                {/* Product description */}
+                                <div className="grid gap-2">
+                                    <Label htmlFor="name">Product Name</Label>
+                                    <Input id='name' name='name' type='text' placeholder='Enter product name' autoFocus tabIndex={1} />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                </Card>
             </div>
         </AppLayout>
     );
